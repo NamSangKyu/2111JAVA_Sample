1b8f3a64b909de37ee7270543be31765c7b1f51a
Elzenaro <nam2626@gmail.com>
2021-11-11 16:37:14 +0900

    주석, 변수

---
+++ Step01/src/Comment.java
@@ -0,0 +1,20 @@
+
+public class Comment {
+	//주석 : 소스코드에 삽입된 코드지만 실행이 안되는 코드
+	//	    해당 소스코드에 대한 설명
+	// 한줄단위 주석
+	/*
+	 	여러줄 단위 주석
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World1");
+//		System.out.println("Hello World2");
+		System.out.println("Hello World3");
+		/*
+		 * System.out.println("Hello World4");
+		 * System.out.println("Hello World5");
+		 */ 
+		
+	}
+
+}
---
+++ Step01/src/PrintValue.java
@@ -0,0 +1,14 @@
+
+public class PrintValue {
+	/*
+	 *	데이터 타입
+	 *		정수, 실수, 문자열, 논리형
+	 */
+	public static void main(String[] args) {
+		System.out.println(100);//정수형
+		System.out.println(3.1415);//실수형
+		System.out.println("String");//문자열
+		System.out.println(false);//논리형
+	}
+
+}
