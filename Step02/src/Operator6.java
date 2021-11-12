ea80dd66ce127f22f80f82fec528854962fee8f1
Elzenaro <nam2626@gmail.com>
2021-11-12 16:11:45 +0900

    삼항연산자

---
+++ Step02/src/Operator6.java
@@ -0,0 +1,12 @@
+
+public class Operator6 {
+
+	public static void main(String[] args) {
+		int n = 10;
+		
+		String result = n % 2 == 0 ? "짝수" : "홀수";
+		System.out.println(result);
+		
+	}
+
+}
---
+++ Step02/src/Quest.java
@@ -0,0 +1,15 @@
+
+public class Quest {
+
+	public static void main(String[] args) {
+		/*
+		 * 정수형 변수 두개 선언하여 임의값으로 초기화를 하고
+		 * 두 변수 값을 뺄셈을 하였을때 무조건 양수가 나오게끔 실행 
+		 */
+		int n1 = 15, n2 = 10;
+		int result = n1 > n2 ? n1 - n2: n2 - n1;
+		System.out.println(result);
+		
+	}
+
+}
