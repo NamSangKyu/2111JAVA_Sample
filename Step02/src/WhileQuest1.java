08be0399916edeb889aa1bf91adc1c75f3d5dcc5
Elzenaro <nam2626@gmail.com>
2021-11-12 16:27:37 +0900

    While문

---
+++ Step02/src/WhileQuest1.java
@@ -0,0 +1,13 @@
+
+public class WhileQuest1 {
+	public static void main(String[] args) {
+		/*
+		 * 1부터 10까지 숫자를 출력하는 반복문을 작성
+		 */
+		int i = 1;
+		while(i < 11) {
+			System.out.println(i);
+			i++;
+		}
+	}
+}
