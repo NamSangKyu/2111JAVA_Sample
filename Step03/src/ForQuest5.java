49cc511863d8be5d10082f043284048f1af6aa46
Elzenaro <nam2626@gmail.com>
2021-11-15 17:37:08 +0900

    For문 예제

--- Step03/src/ForQuest5.java
+++ Step03/src/ForQuest5.java
@@ -1,7 +1,11 @@
 
 public class ForQuest5 {
-	//구구단 2단부터 9단까지 출력
+	// 구구단 2단부터 9단까지 출력
 	public static void main(String[] args) {
-		
+		for (int dan = 2; dan < 10; dan++) {
+			for (int is = 1; is < 10; is++) {
+				System.out.println(dan + " * " + is + " = " + dan * is);
+			}
+		}
 	}
 }
