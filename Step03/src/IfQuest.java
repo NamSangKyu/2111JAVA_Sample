76e72ca50f9c42fed52e6ee6e40a4bfcb56b961a
Elzenaro <nam2626@gmail.com>
2021-11-15 17:47:18 +0900

    If 문 예제

--- Step03/src/IfQuest.java
+++ Step03/src/IfQuest.java
@@ -1,11 +1,13 @@
 
 public class IfQuest {
 	/*
-	 * 	1~100까지 숫자들 중
-	 *  3과 4의 배수를 출력
-	 *  단 3과 4의 공배수는 한번만 출력
+	 * 1~100까지 숫자들 중 3과 4의 배수를 출력
+	 * 단 3과 4의 공배수는 한번만 출력
 	 */
 	public static void main(String[] args) {
-		
+		for (int i = 1; i <= 100; i++) {
+			if(i % 3 == 0 || i % 4 == 0)
+				System.out.println(i);
+		}
 	}
 }
