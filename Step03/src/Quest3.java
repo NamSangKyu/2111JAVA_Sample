a0d0b025f3faab92305751859f788d1d91470c0a
Elzenaro <nam2626@gmail.com>
2021-11-15 15:50:20 +0900

    반복문 중첩

--- Step03/src/Quest3.java
+++ Step03/src/Quest3.java
@@ -10,7 +10,23 @@ public class Quest3 {
 	 *  	반복문 3개가 나옴
 	 */
 	public static void main(String[] args) {
-		
+		int i=0,j=0;
+		while(i<5) {
+			j=0;
+			while(j<i) {
+				System.out.print(" ");
+				j++;
+			}
+			j=0;
+			while(j<(5-i-1)*2+1) {
+				System.out.print("*");
+				j++;
+			}
+			System.out.println();
+			i++;
+		}
 	}
 
 }
+
+
