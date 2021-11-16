2c5c248461d86e140cead53dc1d1da803b4512f8
Elzenaro <nam2626@gmail.com>
2021-11-16 17:46:43 +0900

    배열 예제

--- Step04/src/ArrayQuest2.java
+++ Step04/src/ArrayQuest2.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class ArrayQuest2 {
 
@@ -7,6 +8,24 @@ public class ArrayQuest2 {
 		 * 배열에 내용을 전부 입력 받은 후
 		 * 3의 배수 합을 출력
 		 */
+		Scanner sc = new Scanner(System.in);
+		int[] arr = new int[10];
+		int total = 0;
+		for(int i=0;i<arr.length;i++) {
+			System.out.print("숫자 입력 : ");
+			arr[i] = sc.nextInt();
+		}
+		
+		for(int i=0;i<arr.length;i++) {
+			if(arr[i] % 3 == 0)
+				total += arr[i];
+		}
+		
+		System.out.println("3의 배수의 합 : " + total);
 	}
 
 }
+
+
+
+
